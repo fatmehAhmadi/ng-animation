b98f37d623d375a4916222364bdaf0cf8dc2a733
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-04-08 15:49:13 +0330

    animate square

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('divAnim', [
+      state(
+        'normal',
+        style({ backgroundColor: 'red', transform: 'translateX(0)' })
+      ),
+      state(
+        'highlighted',
+        style({ backgroundColor: 'blue', transform: 'translateX(100px)' })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(300)),
+    ]),
+  ],
 })
 export class AppComponent {
+  state = 'normal';
   title = 'ng-animation';
+
+  onAnimate() {
+    this.state == 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
+  }
 }
