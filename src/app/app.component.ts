37375d56949c83d2083122978fab90ec3454b65c
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-04-09 13:55:31 +0330

    shrink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,18 +21,46 @@ import { Component } from '@angular/core';
         'highlighted',
         style({ backgroundColor: 'blue', transform: 'translateX(100px)' })
       ),
-      transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(300)),
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+
+    trigger('divShrink', [
+      state(
+        'normal',
+        style({ backgroundColor: 'red', transform: 'translateX(0)' })
+      ),
+      state(
+        'highlighted',
+        style({ backgroundColor: 'blue', transform: 'translateX(100px)' })
+      ),
+      state(
+        'shrink',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px) scale(0.5)',
+        })
+      ),
+      transition('normal <=> highlighted', animate(300)),
+      transition('shrink <=> *', animate(300)),
     ]),
   ],
 })
 export class AppComponent {
   state = 'normal';
+  shrinkState = 'normal';
   title = 'ng-animation';
 
   onAnimate() {
     this.state == 'normal'
       ? (this.state = 'highlighted')
       : (this.state = 'normal');
+
+    this.shrinkState == 'normal'
+      ? (this.shrinkState = 'highlighted')
+      : (this.shrinkState = 'normal');
+  }
+
+  onShrink() {
+    this.shrinkState = 'shrink';
   }
 }
